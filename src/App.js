24eb27585a1084ed296728870855aa50b782da83
youssra <youssramortai01@gmail.com>
2023-12-15 22:50:18 +0100

    statische voor inlog erin
    heb even alleen de basic html erin gezet zodat ik er later verder de styling en js erop kan gooien

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div>
+      <header>
+        <h1>UNC INC</h1>
+        <a href="/">menu</a>
       </header>
+      <form action="/">
+        <label htmlFor="fname">First name:</label>
+        <br />
+        <input type="text" id="fname" name="fname" value="John" />
+        <br />
+        <label htmlFor="lname">Last name:</label>
+        <br />
+        <input type="text" id="lname" name="lname" value="Doe" />
+        <br />
+        <br />
+        <input type="submit" value="Submit" />
+      </form>
     </div>
   );
 }
