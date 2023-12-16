3275916dcb64f69944b6be0dc36d56b0d3a447d2
youssra <youssramortai01@gmail.com>
2023-12-16 10:32:55 +0100

    styling van menu

--- src/App.js
+++ src/App.js
@@ -2,24 +2,10 @@ import "./App.css";
 
 function App() {
   return (
-    <div>
-      <header>
-        <h1>UNC INC</h1>
-        <a href="/">menu</a>
-      </header>
-      <form action="/">
-        <label htmlFor="fname">First name:</label>
-        <br />
-        <input type="text" id="fname" name="fname" value="John" />
-        <br />
-        <label htmlFor="lname">Last name:</label>
-        <br />
-        <input type="text" id="lname" name="lname" value="Doe" />
-        <br />
-        <br />
-        <input type="submit" value="Submit" />
-      </form>
-    </div>
+    <header>
+      <h1>UNC INC</h1>
+      <a href="/">menu</a>
+    </header>
   );
 }
 
