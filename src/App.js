cb44ffaecfcb93ce6f37cf2216a460d2ac4dc3e3
youssra <youssramortai01@gmail.com>
2023-12-17 00:22:48 +0100

    dropdown menu
    heb het dropdown menu erin gezet en later die stijlen

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
+import React, { useState } from "react";
 import "./App.css";
 
 function App() {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
   return (
     <header>
       <h1>UNC INC</h1>
-      <a href="/">menu</a>
+      <span onClick={toggleMenu}>menu</span>
+      {isMenuOpen && (
+        <div className="dropdown-menu">
+          <ul>
+            <li>Option 1</li>
+            <li>Option 2</li>
+            <li>Option 3</li>
+          </ul>
+        </div>
+      )}
     </header>
   );
 }
